054d4a159abbc0ffdcbd74c525b6cc4ea4aed5d7
Lemon-zeniye <endalklemon.abate@gmail.com>
2021-12-11 00:08:24 -0800

    second edit on me-style.jsx

--- src/Component/Me/Me-style.jsx
+++ src/Component/Me/Me-style.jsx
@@ -2,13 +2,16 @@ import styled from "styled-components";
 
 export const MeContainer = styled.div`
     min-height: 100vh;
-    /* padding-top: 7rem; */
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-end;
+    @media screen and (max-width: 900px) and (min-width: 600px){
+        background-color: red;
+    }
     @media(max-width: 600px){
         min-height: 80vh;
+        background-color: green;
     }
 `
 
@@ -19,14 +22,21 @@ export const H1 = styled.h1`
     color: ${({night}) => night ? "white": "black"};
     text-align: center;
     padding: 0 4rem 3rem 4rem;
-    @media(max-width: 800px){
+    @media screen and (max-width: 900px) and (min-width: 600px){
+        padding: 0 3rem 2rem 3rem;
+        font-size: 1.8rem;
+    }
+    @media(max-width: 600px){
         padding: 0 2rem 2rem 2rem;
         font-size: 1.5rem;
     }
 `
 export const Img = styled.img`
     height: 60vh;
-    @media(max-width: 800px){
+    @media screen and (max-width: 900px) and (min-width: 600px){
+        height: 50vh;
+    }
+    @media(max-width: 600px){
         height: 40vh;
     }
 `
